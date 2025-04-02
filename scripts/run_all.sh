d9fb5774389af0787b159247ffb9f3915a50639a
someoneintheworlddd <134784218+someoneintheworlddd@users.noreply.github.com>
2025-04-02 16:46:04 +0700

    Update run_all.sh

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -5,7 +5,7 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
-pkill -9 -f spring-petclinic || echo "Failed to kill any apps"
+pkill -9 -f spring-petclinic || echo "Failed to kill any appsssssssssssssss"
 
 docker compose kill || echo "No docker containers are running"
 
